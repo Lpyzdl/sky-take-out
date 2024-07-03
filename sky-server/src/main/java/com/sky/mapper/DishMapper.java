1fe98b683a80c4bb5878556f9ee6cafd36bd5f04
luojie <2587018897@qq.com>
2024-07-03 13:44:34 +0800

    店铺营业状态设置及查询业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -74,6 +74,11 @@ public interface DishMapper {
      */
     List<Dish> list(Dish dish);
 
+    /**
+     * 根据套餐id查看对应菜品
+     * @param setmealId
+     * @return
+     */
     @Select("select a.* from dish a left join setmeal_dish b on a.id = b.dish_id where b.setmeal_id = #{setmealId}")
     List<Dish> getBySetmealId(Long setmealId);
 
