8514ef3d2951168e97cbb7d6ed88c4566ad29a18
luojie <2587018897@qq.com>
2024-07-07 14:14:03 +0800

    清空购物车功能代码开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -88,4 +88,12 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         return list;
 
     }
+
+    /**
+     * 清空购物车
+     */
+    public void cleanShoppingCart() {
+        Long userId = BaseContext.getCurrentId();
+        shoppingCartMapper.deleteByUserId(userId);
+    }
 }
